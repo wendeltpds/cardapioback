72c08e467ba21651f8bd15fd8407c128c9a01221
wendel tadeu <wendel.tadell1@gmail.com>
2024-03-20 18:17:25 -0300

    push

---
+++ src/main/java/com/example/cardapio/CardapioApplication.java
@@ -0,0 +1,14 @@
+package com.example.cardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class CardapioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/cardapio/conntroller/FoodController.java
@@ -0,0 +1,36 @@
+package com.example.cardapio.conntroller;
+
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
+import com.example.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+
+    @Autowired(required = false)
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        this.repository.save(foodData);
+        return;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = this.repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
+}
